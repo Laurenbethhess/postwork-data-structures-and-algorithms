5ca4c7f8c020bf2314ca947355afb363d1ca7739
Lauren Hess <laurenbethhess@gmail.com>
2022-02-28 13:28:07 -0500

    done

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -1,5 +1,13 @@
+require 'set'
+
 def find_first_duplicate(arr)
-  # type your code in here
+  uniques = Set.new
+
+  arr.each do |value|
+    return value if uniques.include?(value)
+    uniques.add(value)
+  end
+  -1
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +24,10 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# I want to take an array and compare each of the indexes
+# until I find an index that matches 
+# then return that index value
+# to do this I create a new Set, which only hold unique values
+# therefore if that value is already in the Set, it can't be added
+# and we have out duplicated value returned
+# if there are no duplicated, and return -1 
