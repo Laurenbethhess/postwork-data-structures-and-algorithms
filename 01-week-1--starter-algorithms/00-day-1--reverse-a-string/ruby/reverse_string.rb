bead68649b22a3290813f2c36538c558b6bfaa8f
Lauren Hess <laurenbethhess@gmail.com>
2022-02-28 13:07:00 -0500

    done

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,12 +1,20 @@
 def reverse_string(str)
-  # type your code in here
+  string = ''
+
+  str.chars.each do |char| 
+    string = char + string
+  end
+
+  string
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
+  puts "Expecting: 'sivam'"
+  puts "=>", reverse_string('mavis')
 
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
@@ -14,5 +22,9 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
+# define an empty string
+# for each charachter in the param string provided,
+# iterate over the charater and add it to the beginggin of the empty string variable
+# then return the (now not empty) string
 # Please add your pseudocode to this file
 # And a written explanation of your solution
