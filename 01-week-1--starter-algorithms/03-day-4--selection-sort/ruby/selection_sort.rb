78ba2b4a39ca894be7129f77298670dec64b16f4
Lauren Hess <laurenbethhess@gmail.com>
2022-03-03 12:35:42 -0500

    done

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
@@ -1,5 +1,19 @@
 def selection_sort(arr)
-  # type your code in here
+  arr.sort
+end
+
+def selection_sort(arr)
+  sorted = []
+  until arr.length === 0
+    time_start = Time.now
+    min = arr.min
+    index = arr.index(min)
+    sorted << min
+    arr.delete_at(index)
+  end
+  time_end = Time.now
+  return sorted
+  puts time_start - time_end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -19,3 +33,7 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+start with an empty array that we will push out numbers into
+first find the minimun number and index, add it to the the empty array, then delete 
+it from the current array, repeat and then return the now sorted array
